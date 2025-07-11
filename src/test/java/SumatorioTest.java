da400552addb68c5f4d2bec10aac5968ded7007e
HeDichoQueNo <no@mail.no>
2025-07-11 12:25:15 +0200

    Ciclos y arrays

---
+++ src/test/java/SumatorioTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+public class SumatorioTest {
+    @Test
+    void testLanzar() {
+
+        Sumatorio sumatorio = new Sumatorio();
+        sumatorio.lanzar();
+
+    }
+}
